1080664b5e5a0bffc8355ee17c61994a8a1bc127
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-08 20:42:21 +0100

    set static folder

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path =require('path')
 const app=express()
 
 //set static folder
-app.use(express.static())
+app.use(express.static(path.join(__dirname,'/public')))
 const PORT = process.env.PORT || 5000
 app.listen(PORT ,()=>{
     console.log('Server starting on the PORT '+PORT)
