512223817b788b6992d1018bb8156907dd305712
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-08 20:24:53 +0100

    install nodemon into the project

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express =require('express')
 const app=express()
+app.get('/',(req,res)=>{
+    res.send('<h1>Home Page</h1>')
+})
 const PORT = process.env.PORT || 5000
 app.listen(PORT ,()=>{
     console.log('Server starting on the PORT '+PORT)
