6e3307db3e79a0e1a315235f656a4947da348e70
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-08 21:09:23 +0100

    create a file for members informations

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const members = [
-  {
-    id: 1,
-    name: "John Doe",
-    email: "john@gmail.com",
-    status: "active",
-  },
-  {
-    id: 2,
-    name: "Bob Williams",
-    email: "bob@gmail.com",
-    status: "inactive",
-  },
-  {
-    id: 3,
-    name: "Shannon Jackson",
-    email: "shannon@gmail.com",
-    status: "active",
-  },
-];
+
 
 //get all members
 app.get("/api/members", (req, res) => {
