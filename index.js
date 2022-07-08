5d6e2c469ef0fefac4ff13fc7541e6416ef66e5e
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-08 21:33:01 +0100

    get single member

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const logger = require("./middleware/logger");
 const app = express();
 
 //init middlware
-app.use(logger)
+// app.use(logger)
+
+//get a single member
+app.get("/api/members/:id", (req, res) => {
+    res.json(members.filter(member=>member.id===parseInt(req.params.id)))
+  });
 
 //get all members
 app.get("/api/members", (req, res) => {
