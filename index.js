703a00a6741059b890766cfb9e6f556ba36fbbc0
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-08 21:06:55 +0100

    create api for get all members

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const express =require('express')
-const path =require('path')
-const app=express()
+const express = require("express");
+const path = require("path");
+const app = express();
+const members = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Bob Williams",
+    email: "bob@gmail.com",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "Shannon Jackson",
+    email: "shannon@gmail.com",
+    status: "active",
+  },
+];
+
+//get all members
+app.get("/api/members", (req, res) => {
+  res.json(members);
+});
 
 //set static folder
-app.use(express.static(path.join(__dirname,'/public')))
-const PORT = process.env.PORT || 5000
-app.listen(PORT ,()=>{
-    console.log('Server starting on the PORT '+PORT)
-})
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server starting on the PORT " + PORT);
+});
